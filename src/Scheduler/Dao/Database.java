d69edee9828d0c812ab2dfef879a640e7beb24c5
Mario Menjívar <mariomenjr@gmail.com>
2020-04-14 23:28:12 -0700

    Add isConnected flag in Database Facade

--- src/Scheduler/Dao/Database.java
+++ src/Scheduler/Dao/Database.java
@@ -15,19 +15,22 @@ public class Database {
     private static final String password = "";
     private static final String hostName = "";
     private static final String driver = "com.mysql.jdbc.Driver";
+
     public static Connection conn;
+    public static boolean isConnected = false;
 
     public static void connect() throws ClassNotFoundException, SQLException, Exception {
         Class.forName(driver);
         String connString = "jdbc:mysql://" + hostName + "/" + dbName;
         conn = (Connection) DriverManager.getConnection(connString, userName, password);
+        isConnected = !conn.isClosed();
 
         System.out.println("Successfully connected to " + dbName + "@" + hostName);
     }
 
     public static void disconnect() throws ClassNotFoundException, SQLException, Exception {
         conn.close();
+        isConnected = false;
         System.out.println("Disconnected from " + dbName + "@" + hostName);
     }
-
 }
